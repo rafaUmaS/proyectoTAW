bcd771366febc7a5501fd44bde9076cf0f8a5ead
Rafasaeza <rafasaezarana@gmail.com>
2025-04-04 19:14:23 +0200

    scriptConUsuarios

---
+++ ProyectoMyLetterBox/MyLetterBox/src/main/java/es/uma/demospring/myletterbox/controller/UserController.java
@@ -0,0 +1,7 @@
+package es.uma.demospring.myletterbox.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+}
---
+++ scriptGeneracionUsuarios.sql
@@ -0,0 +1,7 @@
+use myletterbox;
+INSERT INTO usuario (username, email, password, rol) VALUES
+('rafa', 'prueba', 'prueba', 'administrador'),
+('ivan', 'prueba', 'prueba', 'analista'),
+('gregorio', 'prueba', 'prueba', 'usuario'),
+('adri', 'prueba', 'prueba', 'recomendador'),
+('alvaro', 'prueba', 'prueba', 'editor');
