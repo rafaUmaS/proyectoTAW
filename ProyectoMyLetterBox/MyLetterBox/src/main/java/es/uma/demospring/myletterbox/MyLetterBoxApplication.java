afe901c65d2c8f25aeb4cf670dc599c10d5956fe
IvanPD2002 <100696419+IvanPD2002@users.noreply.github.com>
2025-04-04 17:44:36 +0200

    proyecto springboot

---
+++ ProyectoMyLetterBox/MyLetterBox/src/main/java/es/uma/demospring/myletterbox/MyLetterBoxApplication.java
@@ -0,0 +1,13 @@
+package es.uma.demospring.myletterbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLetterBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLetterBoxApplication.class, args);
+	}
+
+}
---
+++ ProyectoMyLetterBox/MyLetterBox/src/test/java/es/uma/demospring/myletterbox/MyLetterBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uma.demospring.myletterbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLetterBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
